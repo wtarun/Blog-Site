ffbf7f8682f5ac6e47c8fc354ba9e67c9dc94962
wtarun <wajjalatarun@gmail.com>
2022-06-20 22:01:27 +0530

    Implemented ISR

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,7 +41,7 @@ function Home ({ posts }: Props) {
             <div className='flex justify-between p-5 bg-white'>
               <div>
                 <p className='text-lg font-bold'>{post.title}</p>
-                <p className='text-xs'>{post.description} by {post.author.name}</p>
+                <p className='text-xs'>{post.description} by <span className='font-semibold'>{post.author.name}</span></p>
               </div>
 
               <img className='h-12 w-12 rounded-full object-cover' src={urlFor(post.author.image).url()} alt="author-image" />
---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,74 @@
+import { GetStaticProps } from "next"
+import Header from "../../components/Header"
+import { sanityClient, urlFor } from "../../sanity"
+import { Post } from "../../typings"
+
+interface Props {
+    post: Post
+}
+
+const Post = ({ post }: Props) => {
+    return (
+        <main>
+            <Header />
+        </main>
+    )
+}
+
+export default Post
+
+export const getStaticPaths = async () => {
+    const query = `*[_type == "post"] {
+        _id,
+        slug {
+            current
+        }
+    }`
+    const posts = await sanityClient.fetch(query)
+
+    const paths = posts.map((post: Post) => ({
+        params: {
+            slug: post.slug.current
+        }
+    }))
+
+    return {
+        paths,
+        fallback: 'blocking'
+    }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const query = `
+    *[_type == 'post' && slug.current == $slug][0] {
+        _id,
+        title,
+      author -> {
+        name,
+        image
+      },
+      description,
+      mainImage,
+      slug,
+      body
+    }
+    `
+
+    const post = await sanityClient.fetch(query, {
+        slug: params?.slug,
+    })
+
+    if(!post) {
+        return {
+            notFound: true
+        }
+    }
+
+    return {
+        props: {
+            post
+        },
+
+        revalidate: 60*60*24
+    }
+}
